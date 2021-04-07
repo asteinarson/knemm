48a744b77b20219a4c75e969e5f1f1980e65f452
Arne Steinarson <asteinarson@gmail.com>
2021-04-07 13:27:46 +0200

    Moved utility fns to own files

--- src/cmd.ts
+++ src/cmd.ts
@@ -42,7 +42,6 @@ cmd.parse(process.argv);
 // This works for ES module 
 import knex, { Knex } from 'knex';
 
-
 let knex_conn: Knex;
 export async function connect(connection: Record<string, string>, client = "pg") {
     let conn = {
@@ -51,70 +50,6 @@ export async function connect(connection: Record<string, string>, client = "pg")
     }
     knex_conn = knex(conn);
     return knex_conn;
-}
-
-export function slurpSchema(conn: Knex): object {
-    return null;
-}
-
-import { existsSync, readFileSync } from 'fs';
-import { load } from 'js-yaml';
-
-export function slurpFile(file: string): string | number | object {
-    if (existsSync(file)) {
-        let s = readFileSync(file);
-        let ejs = file.slice(-5);
-        try {
-            if (ejs == ".json" || ejs == ".JSON") {
-                return JSON.parse(s.toString());
-            }
-            if (ejs == ".yaml" || ejs == ".YAML") {
-                return load(s.toString());
-            }
-            // No extension match, try the two formats 
-            let r = JSON.parse(s.toString());
-            if(r) return r;
-            r = load(s.toString());
-            if(r) return r;
-        } catch (e) {
-            console.log(`slurpFile ${file}, exception: ${e.toString()}`);
-        }
-    }
-    return null;
-}
-
-
-export async function toNestedDict(file_or_db: string): Promise<string | number | object> {
-    let conn_info: Record<string, string>;
-    if (file_or_db == "@") {
-        // Accept it as a file / link
-        let r = slurpFile("./@");
-        if (typeof r == "object") conn_info = r as Record<string, string>;
-    }
-    if (file_or_db.slice(0, 3) == "db:") {
-        // Look for a connection file
-        let r = slurpFile(file_or_db.slice(3));
-        if (typeof r == "object") conn_info = r as Record<string, string>;
-    }
-    if (file_or_db == "-") {
-        // Use ENV vars
-        let conn_info = {
-            host: process.env.HOST,
-            user: process.env.USER,
-            password: process.env.PASSWORD,
-            database: process.env.DATABASE,
-        }
-    }
-    if (conn_info) {
-        let client = conn_info.client ? conn_info.client : "pg";
-        let connection = conn_info.connection ? conn_info.connection as any as Record<string, string> : conn_info;
-        return slurpSchema(await connect(connection, client));
-    }
-
-    // See if it is a file
-    let r = slurpFile(file_or_db);
-    if (!r) console.log("toNestedDict - file not found: " + file_or_db);
-    return r;
 }
 
 function handle(cmd: string, target: string, candidate: string, options: any) {
