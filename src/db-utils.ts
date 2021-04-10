77444a37b1f5bc510915f3c04a929c93f341b4ce
Arne Steinarson <asteinarson@gmail.com>
2021-04-10 14:12:12 +0200

    More pruning inspector results

--- src/db-utils.ts
+++ src/db-utils.ts
@@ -13,17 +13,39 @@ export async function connect(connection: Record<string, string>, client = "pg")
 }
 
 
+import { Dict } from "./utils.js";
+
 // column props that are by default true 
-import {Dict} from "./utils.js";
-const default_true_props:Dict<boolean> = {}; 
+const default_true_props: Dict<boolean> = {};
+
+// data type translations 
+const data_type_remap: Dict<string> = {
+    "timestamp with time zone": "timestamp_tz",
+    "timestamp without time zone": "timestamp",
+    "character varying": "varchar",
+    "integer": "int"
+}
+
+// Default values for types, which can be dropped in output 
+const default_type_vals: Dict<Dict<string | number>> = {
+    varchar: {
+        max_length: 255
+    },
+    int: {
+        numeric_precision: 32
+    },
+    real: {
+        numeric_precision: 24
+    }
+}
 
 import schemaInspector from 'knex-schema-inspector';
 export async function slurpSchema(conn: Knex, includes?: (string | RegExp)[], excludes?: (string | RegExp)[])
     : Promise<Record<string, any>> {
     // Workaround for ESM import 
-    let sI:any;
-    if( typeof schemaInspector!="function" ){
-        sI = (schemaInspector as any).default(conn);        
+    let sI: any;
+    if (typeof schemaInspector != "function") {
+        sI = (schemaInspector as any).default(conn);
     } else {
         sI = schemaInspector(conn);
     }
@@ -39,7 +61,7 @@ export async function slurpSchema(conn: Knex, includes?: (string | RegExp)[], ex
     for (let tn of tables) {
         let do_exclude = false;
         for (let e of excludes) {
-            if ((typeof e == "string" && tn.indexOf(e)>=0) ||
+            if ((typeof e == "string" && tn.indexOf(e) >= 0) ||
                 (e instanceof RegExp && tn.match(e))) {
                 do_exclude = true;
                 break;
@@ -54,18 +76,32 @@ export async function slurpSchema(conn: Knex, includes?: (string | RegExp)[], ex
             for (let c of columns) {
                 if (c.name && c.data_type) {
                     // Simplifications 
-                    if( c.data_type=='integer' && c.numeric_precision==32 ) 
-                        delete c.numeric_precision; 
-                    if( c.is_primary_key && c.is_unique )
+                    let type = c.data_type;
+                    // Rename some data type ?
+                    if (data_type_remap[type]) {
+                        c.data_type = data_type_remap[type];
+                        type = c.data_type;
+                    }
+                    // Can drop some default value for datatype ? 
+                    if (default_type_vals[type]) {
+                        for (let [k, v] of Object.entries(default_type_vals[type])) {
+                            if (c[k] == v)
+                                delete c[k];
+                        }
+                    }
+                    if (c.is_primary_key && c.is_unique)
                         delete c.is_unique;
                     // Delete properties set to null or false 
-                    for( let k in c ){
-                        if( c[k]==null || (c[k]==false && !default_true_props[k]) )
+                    for (let k in c) {
+                        if (c[k] == null || (c[k] == false && !default_true_props[k]))
                             delete c[k];
                     }
                     // Delete the schema property ? 
-                    if( conn.client.config.client=="pg" && c.schema=="public" )
+                    if (conn.client.config.client == "pg" && c.schema == "public") {
                         delete c.schema;
+                        if (c.foreign_key_schema == "public")
+                            delete c.foreign_key_schema;
+                    }
                     delete c.table;
                     t[c.name] = c;
                     delete c.name;
