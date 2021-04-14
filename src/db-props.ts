e63c0a59c021a5aff89bbabcf15e01b3e4cea11a
Arne Steinarson <asteinarson@gmail.com>
2021-04-14 18:20:15 +0200

    Added getTypeGroup fn

--- src/db-props.ts
+++ src/db-props.ts
@@ -2,10 +2,12 @@
 import { Dict } from './utils.js';
 
 const numerics: Dict<number> = {
-    float: 1, real: 1, int: 1, integer: 1, decimal: 1, numeric: 1
+    float: 1, real: 1, int: 1, integer: 1, bigint: 1, decimal: 1, numeric: 1
 };
 
 const strings: Dict<number> = { text: 1, varchar: 1 };
+
+const datetimes: Dict<number> = { date: 1, time:1, datetime: 1, timestamp:1, timestamp_tz:1 };
 
 export function isNumeric(type: string) {
     return numerics[type];
@@ -15,3 +17,12 @@ export function isString(type: string) {
     return strings[type];
 }
 
+export function getTypeGroup( type:string ): "numeric"|"text"|"json"|"datetime"|"boolean"|"uuid" { 
+    if( numerics[type] ) return "numeric";
+    if( strings[type] ) return "text";
+    if( datetimes[type] ) return "datetime";
+    if( type=="json" || type=="jsonb" ) return "json";
+    if( type=="boolean" ) return "boolean";
+    if( type=="uuid" ) return "uuid";
+    return null;
+}
