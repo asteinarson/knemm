646b8dd085d8334744a31702662b652a55f00946
Arne Steinarson <asteinarson@gmail.com>
2021-04-15 14:35:03 +0200

    Added typeContains for datetime

--- src/db-props.ts
+++ src/db-props.ts
@@ -20,7 +20,7 @@ const strings: Dict<number> = { text: 1, varchar: 1 };
 
 const datetimes: Dict<number> = { date: 1, time: 1, datetime: 1, timestamp: 1, timestamp_tz: 1 };
 
-export function getTypeGroup(type: string): "numeric" | "text" | "json" | "datetime" | "boolean" | "uuid" {
+export function getTypeGroup(type: string): "numeric" | "datetime" | "text" | "json" | "boolean" | "uuid" {
     type = tryGet(type, type_synonyms, type);
     if (numerics[type]) return "numeric";
     if (strings[type]) return "text";
@@ -61,35 +61,54 @@ let num_type_tree_loose: Dict<any> = {
     },
 }
 
+let datetime_type_tree_loose: Dict<any> = {
+    timestamp_tz: {
+        timestamp: 1,
+        time_tz: 1,
+    },
+    time_tz: {
+        time: 1,
+    },
+    timestamp: {
+        date: 1,
+    },
+}
+
+// See if a type tree gives that the outer type contains the inner 
 function typeTreeContains(t_outer: string, t_inner: string, type_dict: Dict<any>) {
     let outer = type_dict[t_outer];
-    if (!outer) return false;
-    if (outer[t_inner]) return true;
-    for (let k in outer) {
-        if (typeTreeContains(k, t_inner, type_dict)) return true;
+    if (outer) {
+        if (outer[t_inner]) return true;
+        for (let k in outer) {
+            if (typeTreeContains(k, t_inner, type_dict))
+                return true;
+        }
     }
-    return false;
 }
 
-function adjustNumTypeLoose(t: string) {
-    t = t.toLowerCase();
-    // According to PG docs
-    if (t == "decimal") return "numeric";
-    return t;
-}
-
-export function numTypeContains(t1: string, t2: string) {
-    t1 = adjustNumTypeLoose(t1);
-    t2 = adjustNumTypeLoose(t2);
-    if (t1 == t2) return true;
-    if (typeTreeContains(t1, t2, num_type_tree_loose)) return true;
-}
-
-
-export function typeContains(t1: string, t2: string) {
+// The 'Loose' suffix is here used in the meaning in "what works
+// under most common(normal) circumstances" - i.e. it does does not 
+// assume to hold under all corner cases. An example is the type 
+// 'int' which is said to contain 'int_u'. Of course, it only holds
+// for unsigned integers up to 2^31. But if we regularly (normally) 
+// start to store larger integers beyond that, in such a column, we 
+// normally would have considered migrating to 'bigint' by then. 
+export function typeContainsLoose(t1: string, t2: string) {
+    t1 = tryGet(t1,type_synonyms,t1);
+    t2 = tryGet(t2,type_synonyms,t2);
+    if( t1==t2 ) return true;
     const tg1 = getTypeGroup(t1);
     const tg2 = getTypeGroup(t2);
     if (tg1 == tg2) {
-
+        if( tg1=="numeric"){
+            return typeTreeContains(t1,t2,num_type_tree_loose);
+        }
+        if( tg1=="datetime" ){
+            return typeTreeContains(t1,t2,datetime_type_tree_loose);
+        }
+        if( tg1=="text" ){
+            return t1=="text";
+        }
+        if( tg1=="json" ) return true;
     }
 }
