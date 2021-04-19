39c755498c2b60ca2586a98939a2b7ffb4702cd4
Arne Steinarson <asteinarson@gmail.com>
2021-04-19 13:22:35 +0200

    Added a one sweep multi replace method

---
+++ src/str-utils.ts
@@ -0,0 +1,21 @@
+import { Dict } from './utils.js';
+
+
+function multiReplaceInternal(s: string, dict: Dict<string>, keys: string[], pos: number): string {
+    let w = keys[pos];
+    let parts = s.split(w);
+    if (++pos < keys.length) {
+        for (let ix in parts) {
+            parts[ix] = multiReplaceInternal(parts[ix], dict, keys, pos);
+        }
+    }
+    return parts.join(dict[w]);
+}
+
+
+// Do a one sweep multiple string replacement where substitutions of 
+// previous ones are not input to later ones. 
+export function multiReplace(s: string, dict: Dict<string>) {
+    return multiReplaceInternal(s, dict, Object.keys(dict), 0);
+}
+
