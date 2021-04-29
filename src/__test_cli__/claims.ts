7d487bc2d11d221b3c97482c92f248ec5debad23
Arne Steinarson <asteinarson@gmail.com>
2021-04-29 10:45:23 +0200

    Separate out test claims to own file

---
+++ src/__test_cli__/claims.ts
@@ -0,0 +1,22 @@
+
+export let claim_p1 = {
+    format: "internal",
+    id: {
+        branch: "cmd-join",
+        version: 1
+    },
+    ___tables: {
+        person: {
+            id: {
+                data_type: "int",
+                is_primary_key: true,
+                has_auto_increment: true
+            },
+            name: {
+                data_type: "varchar",
+                max_length: 32,
+            },
+        },
+    }
+};
+
--- src/__test_cli__/cmd-join.ts
+++ src/__test_cli__/cmd-join.ts
@@ -7,33 +7,13 @@ import { load as yamlLoad } from 'js-yaml';
 import { matchDiff } from '../logic';
 
 import {claimsToFile,captureStart,captureStop,fileOf} from './test-utils';
-
-let claim_s1 = {
-    format: "internal",
-    id: {
-        branch: "cmd-join",
-        version: 1
-    },
-    ___tables: {
-        person: {
-            id: {
-                data_type: "int",
-                is_primary_key: true,
-                has_auto_increment: true
-            },
-            name: {
-                data_type: "varchar",
-                max_length: 32,
-            },
-        },
-    }
-};
+import {claim_p1} from './claims';
 
 // Make sure claims are in file formats 
-claimsToFile([claim_s1]);
+claimsToFile([claim_p1]);
 
 captureStart();
-let r = await handleOneArgCmd("join",[fileOf(claim_s1)],{internal:true});
+let r = await handleOneArgCmd("join",[fileOf(claim_p1)],{internal:true});
 let y_s = captureStop();
 console.log( "test return code: " + r );
 
@@ -42,9 +22,9 @@ try {
     if( isDict(o)){
         // The merge should be identical with the one claim 
         let es:string[] = [];
-        let d1 = matchDiff(claim_s1.___tables,o);
+        let d1 = matchDiff(claim_p1.___tables,o);
         if( firstKey(d1) ) es.push("Expected empty diff1");
-        let d2 = matchDiff(o,claim_s1.___tables);
+        let d2 = matchDiff(o,claim_p1.___tables);
         if( firstKey(d2) ) es.push("Expected empty diff2");
 
         if( !es.length ) console.log("success");
