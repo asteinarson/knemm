14a94a7730ff0233b44b96e3c433a94bd086770b
Arne Steinarson <asteinarson@gmail.com>
2021-04-29 10:50:50 +0200

    fmt

--- src/__test_cli__/cmd-join.ts
+++ src/__test_cli__/cmd-join.ts
@@ -6,31 +6,31 @@ import { load as yamlLoad } from 'js-yaml';
 
 import { matchDiff } from '../logic';
 
-import {claimsToFile,captureStart,captureStop,fileOf} from './test-utils';
-import {claim_p1} from './claims';
+import { claimsToFile, captureStart, captureStop, fileOf } from './test-utils';
+import { claim_p1 } from './claims';
 
 // Make sure claims are in file formats 
 claimsToFile([claim_p1]);
 
 captureStart();
-let r = await handleOneArgCmd("join",[fileOf(claim_p1)],{internal:true});
+let r = await handleOneArgCmd("join", [fileOf(claim_p1)], { internal: true });
 let y_s = captureStop();
-console.log( "test return code: " + r );
+console.log("test return code: " + r);
 
 try {
     let o = yamlLoad(y_s);
-    if( isDict(o)){
+    if (isDict(o)) {
         // The merge should be identical with the one claim 
-        let es:string[] = [];
-        let d1 = matchDiff(claim_p1.___tables,o);
-        if( firstKey(d1) ) es.push("Expected empty diff1");
-        let d2 = matchDiff(o,claim_p1.___tables);
-        if( firstKey(d2) ) es.push("Expected empty diff2");
+        let es: string[] = [];
+        let d1 = matchDiff(claim_p1.___tables, o);
+        if (firstKey(d1)) es.push("Expected empty diff1");
+        let d2 = matchDiff(o, claim_p1.___tables);
+        if (firstKey(d2)) es.push("Expected empty diff2");
 
-        if( !es.length ) console.log("success");
-        else es.forEach( e => console.error(e) );
+        if (!es.length) console.log("success");
+        else es.forEach(e => console.error(e));
     }
-}catch(e){
+} catch (e) {
     console.error("Failed yamlLoad");
 }
 
--- src/__test_cli__/test-utils.ts
+++ src/__test_cli__/test-utils.ts
@@ -2,22 +2,19 @@ import { handleNoArgCmd, handleOneArgCmd, handleTwoArgCmd, handleDbCmd } from '.
 import { Dict, firstKey, isDict } from '../utils';
 
 import { dump as yamlDump } from 'js-yaml';
-import { load as yamlLoad } from 'js-yaml';
 
 import path from 'path';
 import os from 'os';
-import { existsSync, readdirSync, mkdirSync, rmSync, copyFileSync, writeFileSync } from 'fs';
-import rimraf from 'rimraf';
-import { matchDiff } from '../logic';
+import { existsSync, mkdirSync, rmSync, writeFileSync } from 'fs';
 
 // Get somewhere to store temporary claims 
 let temp_claim_dir = path.join(os.tmpdir(), "claims");
 if (!existsSync(temp_claim_dir))
     mkdirSync(temp_claim_dir);
 
-export function getClaimDir(){
+export function getClaimDir() {
     return temp_claim_dir;
-}   
+}
 
 export function fileOf(cl: Dict<any>) {
     return path.join(temp_claim_dir, `${cl.id.branch}.${cl.id.version}.yaml`);
@@ -36,22 +33,22 @@ export function claimsToFile(claims: Dict<any>[]) {
 }
 
 let capture = "";
-let old_log:typeof console.log;
+let old_log: typeof console.log;
 
-export function captureStart(){
-    if( old_log ) process.exit(-1);
+export function captureStart() {
+    if (old_log) process.exit(-1);
     capture = "";
-    old_log = console.log; 
-    console.log = function(msg:any){
+    old_log = console.log;
+    console.log = function (msg: any) {
         capture += msg.toString();
         capture += "\n";
     }
 }
 
-export function captureStop(){
-    if( !old_log ) process.exit(-1);
+export function captureStop() {
+    if (!old_log) process.exit(-1);
     console.log = old_log;
-    old_log = null; 
+    old_log = null;
     return capture;
 }
 
