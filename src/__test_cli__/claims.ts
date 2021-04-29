62684c2a6c427305e298b5a76904be2353aee3eb
Arne Steinarson <asteinarson@gmail.com>
2021-04-29 13:41:44 +0200

    More tests

--- src/__test_cli__/claims.ts
+++ src/__test_cli__/claims.ts
@@ -20,3 +20,78 @@ export let claim_p1 = {
     }
 };
 
+export let claim_p2 = {
+    format: "internal",
+    id: {
+        branch: "p",
+        version: 2
+    },
+    ___tables: {
+        person: {
+            name: {
+                // Change the data type 
+                data_type: "text",
+            },
+            age: {
+                // A new column 
+                data_type: "int",
+            },
+        },
+    }
+};
+
+export let state_p2 = {
+    format: "internal",
+    ___tables: {
+        person: {
+            id: {
+                data_type: "int",
+                is_primary_key: true,
+                has_auto_increment: true
+            },
+            name: {
+                data_type: "text"
+            },
+        },
+    }
+};
+
+export let claim_use_p1 = {
+    format: "internal",
+    id: {
+        branch: "up",
+        version: 1
+    },
+    depends: {
+        branch: "p",
+        version: 1
+    },
+    ___tables: {
+        person: {
+            name: {
+                data_type: "text",
+            },
+        },
+    }
+};
+
+
+export let claim_use_p2 = {
+    format: "internal",
+    id: {
+        branch: "up",
+        version: 1
+    },
+    depends: {
+        branch: "p",
+        version: 2
+    },
+    ___tables: {
+        person: {
+            name: {
+                data_type: "text",
+            },
+        },
+    }
+};
+
--- src/__tests__/cmd-join.ts
+++ src/__tests__/cmd-join.ts
@@ -1,5 +1,5 @@
 import { handleNoArgCmd, handleOneArgCmd, handleTwoArgCmd, handleDbCmd } from '../cmd-handlers';
-import { Dict, firstKey, isDict } from '../utils';
+import { Dict, firstKey, isArray, isDict } from '../utils';
 
 import { dump as yamlDump } from 'js-yaml';
 import { load as yamlLoad } from 'js-yaml';
@@ -16,7 +16,7 @@ function logGet() {
 }
 const jest_log = jest.spyOn(console, "log").mockImplementation((v) => { s_log += v.toString() + "\n" });
 
-import { claim_p1 } from '../__test_cli__/claims';
+import { claim_p1, claim_p2, claim_use_p1, claim_use_p2 } from '../__test_cli__/claims';
 
 test("cmd join test - 1", async () => {
     claimsToFile([claim_p1]);
@@ -27,11 +27,61 @@ test("cmd join test - 1", async () => {
     expect(r).toBe(0);
     expect(typeof o).toBe("object");
 
-    if( isDict(o) ){
+    if (isDict(o)) {
         let d1 = matchDiff(claim_p1.___tables, o);
-        expect( firstKey(d1) ).toBeFalsy();
-        let d2 = matchDiff(o,claim_p1.___tables);
-        expect( firstKey(d2) ).toBeFalsy();
-    } 
+        expect(firstKey(d1)).toBeFalsy();
+        let d2 = matchDiff(o, claim_p1.___tables);
+        expect(firstKey(d2)).toBeFalsy();
+    }
     else expect("o_is_dict").toBe(false);
+});
+
+test("cmd join test - 2", async () => {
+    claimsToFile([claim_p1, claim_p2]);
+    let r = await handleOneArgCmd("join", [fileOf(claim_p2), fileOf(claim_p1)], { internal: true });
+    let y_s = logGet();
+    let o = yamlLoad(y_s);
+
+    expect(r).toBe(0);
+    expect(typeof o).toBe("object");
+
+    if (isDict(o)) {
+        expect((o as any)?.person?.name?.data_type).toBe("text");
+        expect((o as any)?.person?.age?.data_type).toBe("int");
+        expect((o as any)?.person?.age?.max_length).toBeFalsy();
+    }
+    else expect("o_is_dict").toBe(false);
+});
+
+test("cmd join test (branch 1) - 3", async () => {
+    // The using (branch) claim should succeed here 
+    claimsToFile([claim_p1, claim_p2, claim_use_p2]);
+    let r = await handleOneArgCmd("join",
+        [fileOf(claim_p2), fileOf(claim_use_p2), fileOf(claim_p1)],
+        { internal: true });
+    let y_s = logGet();
+    let o = yamlLoad(y_s);
+
+    expect(r).toBe(0);
+    expect(isDict(o)).toBe(true);
+
+    if (isDict(o)) {
+        expect((o as any)?.person?.name?.data_type).toBe("text");
+        expect((o as any)?.person?.age?.data_type).toBe("int");
+        expect((o as any)?.person?.age?.max_length).toBeFalsy();
+    }
+    else expect("o_is_dict").toBe(false);
+});
+
+test("cmd join test (branch 2) - 4", async () => {
+    // This should fail - the datatype in p1 does not match
+    claimsToFile([claim_p1, claim_p2, claim_use_p1]);
+    let r = await handleOneArgCmd("join",
+        [fileOf(claim_p2), fileOf(claim_use_p2), fileOf(claim_p1)],
+        { internal: true });
+    let y_s = logGet();
+    let o = yamlLoad(y_s);
+
+    expect(r).toBe(0);
+    expect(isArray(o)).toBe(true);
 });
