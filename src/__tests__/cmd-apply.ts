d94d10a9898ec1ae30f08239f7eec5aca8b1fab3
Arne Steinarson <asteinarson@gmail.com>
2021-05-04 12:10:15 +0200

    New test files for db and apply command tests

---
+++ src/__tests__/cmd-apply.ts
@@ -0,0 +1,4 @@
+// Need a test to be in this file 
+test("claims dummy test", () => {
+    expect(1).toBe(1);
+});
---
+++ src/__tests__/cmd-db.ts
@@ -0,0 +1,5 @@
+
+// Need a test to be in this file 
+test("claims dummy test", () => {
+    expect(1).toBe(1);
+});
