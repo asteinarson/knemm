39be70b8a9dbb67be42923d4db59963101e8ce7e
Arne Steinarson <asteinarson@gmail.com>
2021-05-09 13:39:05 +0200

    Fixed tests

--- src/__tests__/cmd-db.ts
+++ src/__tests__/cmd-db.ts
@@ -5,12 +5,17 @@ import { connect, disconnect, disconnectAll } from '../db-utils';
 
 import * as dotenv from 'dotenv'
 import { Knex } from 'knex';
+import { jestSetLogToFile } from './test-utils';
 dotenv.config();
 
 afterAll(disconnectAll);
 
 // Need a test to be in this file 
 test("DB: create, drop test", async () => {
+    jestSetLogToFile("./log.log","log");
+    jestSetLogToFile("./warn.log","warn");
+    jestSetLogToFile("./error.log","error");
+
     let r: any;
 
     // Drop if exists 
@@ -37,38 +42,38 @@ test("DB: create, drop test", async () => {
     expect(r).toBeFalsy();
 });
 
-// test("DB: multi connect", async () => {
-//     let conn_info = parseDbSpec("%");
-//     expect(isDict(conn_info)).toBe(true);
+test("DB: multi connect", async () => {
+    let conn_info = parseDbSpec("%");
+    expect(isDict(conn_info)).toBe(true);
 
-//     // Make sure the DB:s exist
-//     let conns: Knex[] = [];
-//     for (let ix = 1; ix < 4; ix++) {
-//         let db = "___jt-" + ix;
-//         let r: any = await existsDb(conn_info, db);
-//         if (!r) {
-//             r = await createDb(conn_info, db);
-//             expect(isDict(r)).toBe(true);
-//         }
-//         else expect(r).toBe(true);
+    // Make sure the DB:s exist
+    let conns: Knex[] = [];
+    for (let ix = 1; ix < 4; ix++) {
+        let db = "___jt-" + ix;
+        let r: any = await existsDb(conn_info, db);
+        if (!r) {
+            r = await createDb(conn_info, db);
+            expect(isDict(r)).toBe(true);
+        }
+        else expect(r).toBe(true);
 
-//         // And connect 
-//         conn_info.connection.database = db;
-//         conns[ix] = await connect(conn_info);
-//     }
+        // And connect 
+        conn_info.connection.database = db;
+        conns[ix] = await connect(conn_info);
+    }
 
-//     // And use each connection 
-//     let qs: Knex.Raw[] = [];
-//     for (let ix = 1; ix < 4; ix++) {
-//         let q = conns[ix].raw(`SELECT 1 + ${ix} AS sum`);
-//         qs.push(q);
-//     }
-//     let rr = await Promise.all(qs);
+    // And use each connection 
+    let qs: Knex.Raw[] = [];
+    for (let ix = 1; ix < 4; ix++) {
+        let q = conns[ix].raw(`SELECT 1 + ${ix} AS sum`);
+        qs.push(q);
+    }
+    let rr = await Promise.all(qs);
 
-//     // And close 
-//     for (let ix = 0; ix<rr.length; ix++ ){
-//         let _sum = rr[ix]?.rows?.[0]?.sum;
-//         expect(_sum).toBe(2+ix);
-//         await disconnect(conns[ix+1]);
-//     }
-// });
+    // And close 
+    for (let ix = 0; ix<rr.length; ix++ ){
+        let _sum = rr[ix]?.rows?.[0]?.sum;
+        expect(_sum).toBe(2+ix);
+        await disconnect(conns[ix+1]);
+    }
+});
